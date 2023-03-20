3eb8c8af53b6d90f963c32c0c77b7d699b2eaa1a
Crack-way <rupeshsunuwar2019@gmail.com>
2023-03-20 11:29:46 +0545

    s added

--- src/week_1/day3/F1_Operators.java
+++ src/week_1/day3/F1_Operators.java
@@ -2,7 +2,8 @@ package week_1.day3;
 
 public class F1_Operators {
     public static void main(String[] args) {
-        System.out.println("this is operator");
+
+        System.out.println("this is operators");
     }
 
 }
