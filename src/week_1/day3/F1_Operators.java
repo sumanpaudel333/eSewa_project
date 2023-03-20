ef9074e2f8ead52898f7fdfecb484f77762bc445
Crack-way <rupeshsunuwar2019@gmail.com>
2023-03-20 11:28:03 +0545

    Fuck

--- src/week_1/day3/F1_Operators.java
+++ src/week_1/day3/F1_Operators.java
@@ -2,7 +2,7 @@ package week_1.day3;
 
 public class F1_Operators {
     public static void main(String[] args) {
-        System.out.println("this is operators g");
+        System.out.println("this is operator");
     }
 
 }
