9890faf1e61dcf64a2c4b35d123e2e56b7adf590
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-26 15:30:21 +0545

    iterator

---
+++ src/week_2/day8/cursor/F2_Iterator.java
@@ -0,0 +1,40 @@
+package week_2.day8.cursor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class F2_Iterator {
+
+    // we can apply iterator concept for any collection object hence it is universal cursor
+    // by using iterator we can perform both read and remove operations.
+
+    /*
+        METHODS:
+        public boolean hasNext()
+        public Object next()
+        public void remove()
+
+        Iterator i = c.iterator();
+
+     */
+
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            al.add(i);
+        }
+        System.out.println(al);
+
+        Iterator i = al.iterator();
+        while (i.hasNext()) {
+            Integer i2 = (Integer) i.next();
+            if (i2 % 2 == 0) {
+                System.out.println(i2);
+            } else {
+                i.remove();
+            }
+        }
+        System.out.println(al);
+
+    }
+}
