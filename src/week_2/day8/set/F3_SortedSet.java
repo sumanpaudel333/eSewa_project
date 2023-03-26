992b19bc8ceab07d515faddf288e7f7fec3b85fe
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-26 16:53:19 +0545

    sortedset

---
+++ src/week_2/day8/set/F3_SortedSet.java
@@ -0,0 +1,33 @@
+package week_2.day8.set;
+
+public class F3_SortedSet {
+    /*
+        It is the child interface of set
+        If we want to represent a group of individual objects according to some sorting order and duplicates are not
+        allowed then we should go for SortedSet
+     */
+
+    /*
+        methods:
+
+        first()
+        last()
+        headSet(Object o) -> less than o
+        tailSet(Object o) -> greater than 0
+        subSet(103,110) -> greater than 103 and less than 110
+
+        comparator() -> returns comparator object that describes underlying sorting technique. If we are using
+        default natural sorting order then we will get null
+     */
+
+    /*
+        default natural sorting order
+        - for numbers = ascending
+        - for strings = alphabetical order
+     */
+
+    public static void main(String[] args) {
+
+
+    }
+}
