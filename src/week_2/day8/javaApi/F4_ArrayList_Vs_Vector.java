76bfca58db26a42ef612c764a5f47b4a7ccd454d
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-26 12:03:50 +0545

    arraylist vs vector

---
+++ src/week_2/day8/javaApi/F4_ArrayList_Vs_Vector.java
@@ -0,0 +1,18 @@
+package week_2.day8.javaApi;
+
+public class F4_ArrayList_Vs_Vector {
+
+    /*
+
+        ArrayList               Vector
+
+        non-synchronized        - synchronized
+        not thread-safe         - thread-safe
+        performance is high     - performance is low
+        1.2v                    - 1.0v (legacy)
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
