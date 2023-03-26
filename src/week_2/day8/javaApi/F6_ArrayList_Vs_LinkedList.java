b9ffb244f8b0f089cc70988f1315772ad767a281
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-26 12:47:57 +0545

    AL vs LL

---
+++ src/week_2/day8/javaApi/F6_ArrayList_Vs_LinkedList.java
@@ -0,0 +1,17 @@
+package week_2.day8.javaApi;
+
+public class F6_ArrayList_Vs_LinkedList {
+
+    /*
+        ArrayList                                   LinkedList
+
+        best retrieve                               best choice insert or delete in middle
+        worst insertion and deletion in middle      best retrieve
+        resizable or growable DS                    doubly LL DS
+        implements randomAccess                     doesn't implement randomAccess
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
