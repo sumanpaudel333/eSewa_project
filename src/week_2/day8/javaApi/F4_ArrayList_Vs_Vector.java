98b29c7a8bd26f9ee7e93d07dd9d3f426b5c2464
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-26 12:24:53 +0545

    al vs vector + synchronization

--- src/week_2/day8/javaApi/F4_ArrayList_Vs_Vector.java
+++ src/week_2/day8/javaApi/F4_ArrayList_Vs_Vector.java
@@ -1,5 +1,7 @@
 package week_2.day8.javaApi;
 
+import java.util.*;
+
 public class F4_ArrayList_Vs_Vector {
 
     /*
@@ -13,6 +15,25 @@ public class F4_ArrayList_Vs_Vector {
 
      */
     public static void main(String[] args) {
+
+        // How to get synchronized version of ArrayList Object?
+
+        /*
+            By default, ArrayList is Object is non-synchronized, but we can get synchronized version of ArrayList by
+            using Collection class synchronizedList() method.
+
+         */
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("don");
+        List<String> l = Collections.synchronizedList(arr);
+
+        // public static List synchronizedList(List l)
+
+        // Similarly we can get Synchronized version of Set, Map Objects by using the following methods of Collection
+        // class.
+            // public static Set synchronizedSet(Set s)
+            // public static Map synchronizedSet(Map m)
+
 
     }
 }
